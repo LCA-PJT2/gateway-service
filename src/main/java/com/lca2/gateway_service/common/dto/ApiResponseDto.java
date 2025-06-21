0b7a25b0674a053b3bb98d633e269faa62b0f048
minju26 <mjalswn26@gmail.com>
2025-06-22 06:54:39 +0900

    style: 불필요한 주석 제거, 개행 추가

--- src/main/java/com/lca2/gateway_service/common/dto/ApiResponseDto.java
+++ src/main/java/com/lca2/gateway_service/common/dto/ApiResponseDto.java
@@ -10,7 +10,6 @@ public class ApiResponseDto<T> {
     private String message;
     private T data;
 
-    // private로 만듦. 밖에서 따로 생성하지 않겠다.
     private ApiResponseDto(String code, String message) {
         this.code = code;
         this.message = message;
@@ -22,7 +21,6 @@ public class ApiResponseDto<T> {
         this.data = data;
     }
 
-    // public으로 만들어서, 아래의 것들을 밖에서 쓰겠다.
     public static <T> ApiResponseDto<T> createOk(T data) {
         return new ApiResponseDto<>("OK", "요청이 성공하였습니다.", data);
     }
